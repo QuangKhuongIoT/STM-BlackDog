0c806e298b48775e36214458dbacb2ba4c1d7213
KhuongTran <Khuong.TranQuang@gmail.com>
2022-08-18 16:01:33 +0700

    [Added] generic input button event library

---
+++ blackDog/src/myButton.c
@@ -0,0 +1,132 @@
+/**
+ * @file mybutton.c
+ * @author Tran Quang Khuong (khuong.tranquang@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-17
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "mybutton.h"
+
+void inputConfig(myButton* button, uint8_t activeLevel, v_callbackFunc inputInit, u8_callbackFunc getInputStatus, u32_callbackFunc getCurrentTickMs) {
+    // button->attachClick = inputAttachClick;
+    if (button->_proccessInfo.debounceTimeMs == 0) button->_proccessInfo.debounceTimeMs = 50;
+    if (button->_proccessInfo.clickTimeMs == 0) button->_proccessInfo.clickTimeMs = 400;
+    if (button->_proccessInfo.endActionTimeMs == 0) button->_proccessInfo.endActionTimeMs = 600;
+    if (button->_proccessInfo.pressTimeMs == 0) button->_proccessInfo.pressTimeMs = 1000;
+    if (button->_proccessInfo.longPressTimeMs == 0) button->_proccessInfo.longPressTimeMs = 5000;
+    button->_inputInfo.status.inputConfigSuccess = 0;
+    button->_inputInfo.status.activeLevel = activeLevel;
+    if ((inputInit) && (getInputStatus) && (getCurrentTickMs)) {
+        // button->_inputInfo.ptrInitFn = inputInit;
+        inputInit();
+        // button->_inputInfo.ptrInitFn();
+        button->_inputInfo.ptrGetInputStatusFn = getInputStatus;
+        button->_inputInfo.ptrGetCurrentTickMsFn = getCurrentTickMs;
+        button->_inputInfo.status.inputConfigSuccess = 1;
+    }
+}
+
+void inputTick(myButton* button) {
+    if (!(button->_inputInfo.status.inputConfigSuccess)) return;
+
+    button->_inputInfo.status.logicState = button->_inputInfo.ptrGetInputStatusFn();
+
+    if (button->_inputInfo.status.logicLastState != button->_inputInfo.status.logicState) {
+        button->_inputInfo.status.logicLastState = button->_inputInfo.status.logicState;
+        button->_proccessInfo.startTimeMs = button->_inputInfo.ptrGetCurrentTickMsFn();
+    }
+
+    button->_proccessInfo.actionTimeMs = button->_inputInfo.ptrGetCurrentTickMsFn() - button->_proccessInfo.startTimeMs;
+
+    if (button->_inputInfo.status.logicState == button->_inputInfo.status.activeLevel) {
+        if (button->_proccessInfo.actionTimeMs > button->_proccessInfo.debounceTimeMs) {
+            if (button->_proccessInfo.logicLastState != button->_inputInfo.status.logicState) {
+                button->_proccessInfo.logicLastState = button->_inputInfo.status.logicState;
+                button->_proccessInfo.multiClickCounter++;
+                switch (button->_proccessInfo.multiClickCounter) {
+                    case 0: {
+                        button->_proccessInfo.event = EVENT_STT_UNKNOWN;
+                        break;
+                    }
+                    case 1: {
+                        button->_proccessInfo.event = EVENT_STT_CLICK;
+                        break;
+                    }
+                    case 2: {
+                        button->_proccessInfo.event = EVENT_STT_DOUBLE_CLICK;
+                        break;
+                    }
+                    default:
+                        button->_proccessInfo.event = EVENT_STT_MULTI_CLICK;
+                        break;
+                }
+            } else {
+                if (button->_proccessInfo.multiClickCounter <= 1) {
+                    if ((button->_proccessInfo.actionTimeMs > button->_proccessInfo.pressTimeMs) && (button->_proccessInfo.actionTimeMs < button->_proccessInfo.longPressTimeMs)) {
+                        button->_proccessInfo.event = EVENT_STT_PRESS;
+                    } else if ((button->_proccessInfo.actionTimeMs > button->_proccessInfo.longPressTimeMs) && (button->_proccessInfo.event != EVENT_STT_LONG_PRESS)) {
+                        button->_proccessInfo.event = EVENT_STT_LONG_PRESS;
+                        if (button->_event.ptrLongPressFn) button->_event.ptrLongPressFn();
+                    }
+                }
+            }
+        }
+    } else {
+        if (button->_proccessInfo.actionTimeMs > button->_proccessInfo.debounceTimeMs) {
+            button->_proccessInfo.logicLastState = button->_inputInfo.status.logicState;
+            if (button->_proccessInfo.actionTimeMs > button->_proccessInfo.endActionTimeMs) {
+                button->_proccessInfo.lastMultiClickCounter = button->_proccessInfo.multiClickCounter;
+                button->_proccessInfo.multiClickCounter = 0;
+                if (button->_proccessInfo.event != EVENT_STT_LONG_PRESS) {
+                    switch (button->_proccessInfo.event) {
+                        case EVENT_STT_CLICK: {
+                            if (button->_event.ptrClickFn) button->_event.ptrClickFn();
+                            break;
+                        }
+                        case EVENT_STT_DOUBLE_CLICK: {
+                            if (button->_event.ptrDoubleClickFn) button->_event.ptrDoubleClickFn();
+                            break;
+                        }
+                        case EVENT_STT_MULTI_CLICK: {
+                            if (button->_event.ptrMultiClickFn) button->_event.ptrMultiClickFn();
+                            break;
+                        }
+                        case EVENT_STT_PRESS: {
+                            if (button->_event.ptrPressFn) button->_event.ptrPressFn();
+                            break;
+                        }
+
+                        default:
+                            break;
+                    }
+                } else {
+                    if (button->_event.ptrLongPressReleaseFn) button->_event.ptrLongPressReleaseFn();
+                }
+                button->_proccessInfo.event = EVENT_STT_UNKNOWN;
+            }
+        }
+    }
+}
+
+void inputAttachClick(myButton* button, v_callbackFunc newFunction) {
+    button->_event.ptrClickFn = newFunction;
+}
+void inputAttachDoubleClick(myButton* button, v_callbackFunc newFunction) {
+    button->_event.ptrDoubleClickFn = newFunction;
+}
+void inputAttachMultiClick(myButton* button, v_callbackFunc newFunction) {
+    button->_event.ptrMultiClickFn = newFunction;
+}
+void inputAttachPress(myButton* button, v_callbackFunc newFunction) {
+    button->_event.ptrPressFn = newFunction;
+}
+void inputAttachLongPress(myButton* button, v_callbackFunc newFunction) {
+    button->_event.ptrLongPressFn = newFunction;
+}
+void inputAttachDepress(myButton* button, v_callbackFunc newFunction) {
+    button->_event.ptrLongPressReleaseFn = newFunction;
+}
---
+++ blackDog/src/myButton.h
@@ -0,0 +1,95 @@
+/**
+ * @file myButton.h
+ * @author Tran Quang Khuong (khuong.tranquang@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-17
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __MY_BUTTON_H
+#define __MY_BUTTON_H
+
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*v_callbackFunc)(void);
+typedef uint32_t (*u32_callbackFunc)(void);
+typedef uint8_t (*u8_callbackFunc)(void);
+
+typedef enum {
+    EVENT_STT_UNKNOWN,
+    EVENT_STT_CLICK,
+    EVENT_STT_DOUBLE_CLICK,
+    EVENT_STT_MULTI_CLICK,
+    EVENT_STT_PRESS,
+    EVENT_STT_LONG_PRESS,
+    EVENT_STT_DEPRESS
+} event_state_t;
+
+typedef struct
+{
+    struct {
+        uint8_t activeLevel : 1;            /* Logic level when active action happen */
+        uint8_t logicState : 1;             /* Logic of current status */
+        uint8_t logicLastState : 1;         /* Logic of last status */
+        uint8_t inputLastState : 1;         /* input last status */
+        uint8_t inputConfigSuccess : 1;     /* Flag of initialization successful */
+        uint8_t spare : 3;                  /* Spare bits */
+    } status;                               /* Input status info */
+    v_callbackFunc ptrInitFn;               /* Function pointer for user in order to configuring input */
+    u8_callbackFunc ptrGetInputStatusFn;    /* Function pointer for user tranfer the input status */
+    u32_callbackFunc ptrGetCurrentTickMsFn; /* Function pointer for user tranfer the current system tick counter*/
+} input_info_t;
+
+typedef struct
+{
+    uint8_t logicLastState;         /* Logic of recent event */
+    uint32_t startTimeMs;           /* Timer value at the begining of logic changing */
+    uint32_t actionTimeMs;          /* Timer counter for current logic state status */
+    uint32_t activeTimeMs;          /* Timer counter for recent active logic state status */
+    uint32_t deactiveTimeMs;        /* Timer counter for recent deactive logic state status */
+    uint16_t endActionTimeMs;       /* Timer value to detect an action was finished */
+    uint16_t debounceTimeMs;        /* Setting value to remove noise effect */
+    uint16_t clickTimeMs;           /* Setting value to detect a click action */
+    uint16_t pressTimeMs;           /* Setting value to detect a press action */
+    uint16_t longPressTimeMs;       /* Setting value to detect a long press action */
+    uint16_t multiClickCounter;     /* Counter value contains the number of click actions to return the event respectively */
+    uint16_t lastMultiClickCounter; /* Counter value contains the recent number of click actions */
+    event_state_t event;            /* Recent event detected */
+} process_info_t;
+
+typedef struct
+{
+    v_callbackFunc ptrClickFn;            /* Click trigger event function pointer */
+    v_callbackFunc ptrDoubleClickFn;      /* Double click trigger event function pointer */
+    v_callbackFunc ptrMultiClickFn;       /* Multiple click trigger event function pointer */
+    v_callbackFunc ptrPressFn;            /* Press trigger event function pointer */
+    v_callbackFunc ptrLongPressFn;        /* Long press trigger event function pointer */
+    v_callbackFunc ptrLongPressReleaseFn; /* Long press released trigger event function pointer */
+} event_t;
+
+typedef struct {
+    input_info_t _inputInfo;
+    process_info_t _proccessInfo;
+    event_t _event;
+} myButton;
+
+void inputConfig(myButton *button, uint8_t activeLevel, v_callbackFunc inputInit, u8_callbackFunc getInputStatus, u32_callbackFunc getCurrentTickMs);
+void inputTick(myButton *button);
+void inputAttachClick(myButton *button, v_callbackFunc newFunction);
+void inputAttachDoubleClick(myButton *button, v_callbackFunc newFunction);
+void inputAttachMultiClick(myButton *button, v_callbackFunc newFunction);
+void inputAttachPress(myButton *button, v_callbackFunc newFunction);
+void inputAttachLongPress(myButton *button, v_callbackFunc newFunction);
+void inputAttachDepress(myButton *button, v_callbackFunc newFunction);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
